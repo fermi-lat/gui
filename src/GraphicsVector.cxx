32d9ffe112c72c91f4c3b8ff09e194fb7c9d2c56
cvs2git <>
2002-11-01 22:57:10 +0000

    This commit was manufactured by cvs2svn to create tag 'v3r3p2'.
    Sprout from GLAST 2002-11-01 22:57:09 UTC burnett 'AutoUpdate'
    Cherrypick from master 2002-11-01 22:57:05 UTC burnett '':
        cmt/.cvsignore
        cmt/requirements
        doc/release.notes
        gui/DisplayControl.h
        gui/DisplayList.h
        gui/DisplayRep.h
        gui/GUI.h
        gui/GraphicsVector.h
        gui/GuiMgr.h
        gui/Menu.h
        gui/PrintControl.h
        src/DisplayControl.cxx
        src/DisplayList.cxx
        src/DisplayRep.cxx
        src/GUI.cxx
        src/GraphicsVector.cxx
        src/GuiMgr.cxx
        src/Menu.cxx
        src/MotifGUI.cxx
        src/PrintControl.cxx
        src/SceneControl.cxx
        src/ViewPort.cxx
        src/WinDraw.cxx
        src/WinDraw.h
        src/WinGUI.cxx
        src/WinGUI.h
        src/WinGUIostream.cxx
        src/WinMain.cxx
        src/Xdraw.cxx
        src/Xostream.cxx
        src/Xostream.h
        src/test/Demo.cxx
        src/test/testmain.cxx
    Delete:
        cmt/setup.bat

--- src/GraphicsVector.cxx
+++ src/GraphicsVector.cxx
@@ -2,10 +2,14 @@
 
 #include "gui/GraphicsVector.h"
 #include "CLHEP/Vector/ThreeVector.h"
+#include "CLHEP/Geometry/Point3D.h"
 
 using namespace gui;
 
 // implement conversion constructors here
 
 GraphicsVector::GraphicsVector(const Hep3Vector& v)
+    : m_x(v.x()), m_y(v.y()), m_z(v.z()) {}
+
+GraphicsVector::GraphicsVector(const HepPoint3D& v)
     : m_x(v.x()), m_y(v.y()), m_z(v.z()) {}
