a1c082903ed0a8b099d27dd3c48e3d64ee4d1544
cvs2git <>
2004-04-20 14:19:08 +0000

    This commit was manufactured by cvs2svn to create tag 'v3r6p2'.
    Sprout from GLAST 2004-04-20 14:19:07 UTC burnett 'AutoUpdate'
    Cherrypick from master 2004-04-20 14:19:05 UTC burnett '':
        cmt/.cvsignore
        cmt/requirements
        doc/index.html
        doc/release.notes
        gui/DisplayControl.h
        gui/DisplayList.h
        gui/DisplayRep.h
        gui/GUI.h
        gui/GraphicsVector.h
        gui/GuiMgr.h
        gui/Menu.h
        gui/PrintControl.h
        src/DisplayControl.cxx
        src/DisplayList.cxx
        src/DisplayRep.cxx
        src/GUI.cxx
        src/GraphicsVector.cxx
        src/GuiMgr.cxx
        src/Menu.cxx
        src/MotifGUI.cxx
        src/PrintControl.cxx
        src/SceneControl.cxx
        src/ViewPort.cxx
        src/WinDraw.cxx
        src/WinDraw.h
        src/WinGUI.cxx
        src/WinGUI.h
        src/WinGUIostream.cxx
        src/WinMain.cxx
        src/Xdraw.cxx
        src/Xostream.cxx
        src/Xostream.h
        src/test/Demo.cxx
        src/test/testmain.cxx
    Delete:
        cmt/setup.bat

--- src/DisplayList.cxx
+++ src/DisplayList.cxx
@@ -1,4 +1,4 @@
-//     $Id: DisplayList.cpp,v 1.3 1999/10/22 18:17:13 burnett Exp $
+//     $Id: DisplayList.cxx,v 1.4 2001/10/06 04:22:14 burnett Exp $
 //  Author: Toby Burnett
 //
 //
@@ -315,16 +315,19 @@ void DisplayList::draw(ViewPort* vw)
     if( hidden() || (m_nodelist==0) || (m_nodelist->size()==0) ) return;
 
     DispList::iterator diter = m_nodelist->begin();
-    DisplayList::Node* disp = *diter;
 
+
+#if 0 //THB disable this turkey!
+    DisplayList::Node* disp = *diter;
     // exit if scale is too small: first DisplayList::Node object does it
     if( ! (m_flags & noCheckDetailFlag)  ){
         float size = ((Limits*)disp)->checkScale(vw);
         if( size < 1 && size < vw->detail() ) {
-    	    disp->draw(vw);
-	    return;
-	}
+			disp->draw(vw);
+			return;
+		}
     }
+#endif
     if( selected() )  vw->set_enhanced(1);
 
     // get all the other DisplayList::Node objects to draw themselves
@@ -412,7 +415,7 @@ void DisplayList::indexedLineSet(const int* a, int n)
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void DisplayList::move_to(const GraphicsVector& pt)
+void DisplayList::moveTo(const GraphicsVector& pt)
 //------------------------------------------
 {
         addDisp( new PolyLine(pt) );
@@ -420,7 +423,7 @@ void DisplayList::move_to(const GraphicsVector& pt)
         checkScale(pt);
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void DisplayList::line_to(const GraphicsVector& pt)
+void DisplayList::lineTo(const GraphicsVector& pt)
 //------------------------------------------
 {
     if( status != POLYLINE )
@@ -493,8 +496,8 @@ DisplayList::PolyLine::addPoint(const GraphicsVector& pt)
 
 void
 DisplayList::PolyLine::draw(ViewPort* v)
-{  // N.B.: this only works for std::vector<GraphicsVector>
-    v->drawPL( begin(), size());
+{  // N.B.: note address of dereference
+    v->drawPL( &*begin(), size());
 }
 
 void
@@ -511,8 +514,8 @@ DisplayList::PolyLine::~PolyLine()
 DisplayList::Markers::Markers(const GraphicsVector& pt):PolyLine(pt){}
 
 void DisplayList::Markers::draw(ViewPort* v)
-{
-	v->draw_markers(begin(), size());
+{  // N.B.: note address of dereference
+	v->draw_markers(&*begin(), size());
 }
 //-----------------------------------------------------------------------
 
@@ -586,8 +589,8 @@ DisplayList::CoordList::CoordList(const GraphicsVector& pt):PolyLine(pt){}
 
 void
 DisplayList::CoordList::draw(ViewPort* v)
-{
-   v->coordinate3(begin(), size());
+{  // N.B.: note address of dereference
+   v->coordinate3(&*begin(), size());
 }
 
 
