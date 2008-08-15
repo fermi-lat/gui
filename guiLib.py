76d4f68468e92ff22b76e11adbef8a1e7c7d654b
ecephas <>
2008-08-15 21:42:40 +0000

    Added Lib file (e.g., <package>Lib.py) for each package that creates a library.

--- guiLib.py
+++ guiLib.py
@@ -1,8 +1,8 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/gui/guiLib.py,v 1.1 2008/07/09 21:13:47 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = ['gui', 'guisystem'])
-        env.Tool('addLibrary', library = env['x11Libs'])
+        env.Tool('addLibrary', library = ['gui'])
+	env.Tool('addLibrary', library = ['guisystem'])
 
 def exists(env):
     return 1;
