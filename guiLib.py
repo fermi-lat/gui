3a774c30c306bfe4f24585c8d8473d1c22d71db4
ecephas <>
2008-12-09 00:08:49 +0000

    Check in guiLib.py for gui v3r7p2 (branch-tag, GlastReleasev15r49).

--- guiLib.py
+++ guiLib.py
@@ -1,8 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/gui/guiLib.py,v 1.1 2008/07/09 21:13:47 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/gui/guiLib.py,v 1.2 2008/08/15 21:42:39 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['gui'])
-	env.Tool('addLibrary', library = ['guisystem'])
-
+    env.Tool('addLibrary', library = ['guisystem'])
+    env.Tool('addLibrary', library = env['x11Libs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
