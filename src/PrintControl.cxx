c5ca000bf8666d00c7deea1867a6eadfd6542aa1
cvs2git <>
2010-06-12 22:44:48 +0000

    This commit was manufactured by cvs2svn to create tag 'GlastRelease-17-35-24-gr08'.
    Sprout from GLAST 2010-06-12 22:44:36 UTC jrb 'AutoUpdate'
    Cherrypick from master 2010-06-12 22:44:37 UTC jrb 'stag/GoGui_auto-tag':
        .cvsignore
        SConscript
        cmt/.cvsignore
        cmt/requirements
        doc/blesepa.gif
        doc/index.html
        doc/release.notes
        gui/DisplayControl.h
        gui/DisplayList.h
        gui/DisplayRep.h
        gui/GUI.h
        gui/GraphicsVector.h
        gui/GuiMgr.h
        gui/Menu.h
        gui/PrintControl.h
        guiLib.py
        src/DisplayControl.cxx
        src/DisplayList.cxx
        src/DisplayRep.cxx
        src/GUI.cxx
        src/GraphicsVector.cxx
        src/GuiMgr.cxx
        src/Menu.cxx
        src/MotifGUI.cxx
        src/PrintControl.cxx
        src/SceneControl.cxx
        src/ViewPort.cxx
        src/WinDraw.cxx
        src/WinDraw.h
        src/WinGUI.cxx
        src/WinGUI.h
        src/WinGUIostream.cxx
        src/WinMain.cxx
        src/Xdraw.cxx
        src/Xostream.cxx
        src/Xostream.h
        src/test/Demo.cxx
        src/test/testmain.cxx
    Delete:
        cmt/setup.bat

--- src/PrintControl.cxx
+++ src/PrintControl.cxx
@@ -1,4 +1,4 @@
-//     $Id: PrintControl.cpp,v 1.7 1999/11/09 18:35:58 burnett Exp $
+//     $Id: PrintControl.cxx,v 1.2 2001/01/25 23:08:40 tlindner Exp $
 //  Author: Toby Burnett
 //
 //
@@ -13,9 +13,10 @@
 // the std::ostream* PrintControl::instance()->out(); 
 
 
+#include "gui/SubMenu.h"
 #include "gui/PrintControl.h"
 #include "gui/SimpleCommand.h"
-#include "gui/SubMenu.h"
+
 
 #include <cassert>
 
@@ -46,7 +47,6 @@ PrintCommand::~PrintCommand()
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void PrintControl::setup()
 {
-    s_instance = this;
     disable();
     // create Print pull-down menu
     m_sub_menu = &m_menu.subMenu("Print");
@@ -106,13 +106,5 @@ void PrintControl::clear(){m_print->clear();}
 void PrintControl::flush(){m_print->flush();}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//   stuff for singleton 
-PrintControl* PrintControl::s_instance = 0;
-
-PrintControl* PrintControl::instance()
-{
-    assert( s_instance!=0)	;
-    return s_instance;
-}
 
 } // namespace gui
