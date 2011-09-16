dba56ad313207b31ca93d9ce60d0affacda87140
heather <>
2011-09-16 04:15:02 +0000

    patches for windows

--- SConscript
+++ SConscript
@@ -1,7 +1,7 @@
 # -*- python -*-
-# $Header: /nfs/slac/g/glast/ground/cvs/gui/SConscript,v 1.8 2010/06/12 22:44:34 jrb Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/gui/SConscript,v 1.9 2011/05/21 20:11:51 heather Exp $
 # Authors: T.Burnett <tburnett@u.washington.edu>
-# Version: gui-03-07-05
+# Version: gui-03-07-06
 Import('baseEnv')
 Import('listFiles')
 Import('packages')
--- guiLib.py
+++ guiLib.py
@@ -1,9 +1,12 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/gui/guiLib.py,v 1.2 2008/08/15 21:42:39 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/gui/guiLib.py,v 1.4 2009/01/23 00:21:12 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['gui'])
     env.Tool('addLibrary', library = ['guisystem'])
-    env.Tool('addLibrary', library = env['x11Libs'])
+    if env['PLATFORM'] != 'win32':
+        env.Tool('addLibrary', library = env['x11Libs'])
+    else:
+        env.Tool('addLibrary', library = ['gdi32', 'winspool', 'comdlg32', 'shell32'])
     env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
